81227590c767fd82afe68b737fdf2326a720f31f
Blaine Garrett <blaine@blainegarrett.com>
2021-07-24 14:29:58 -0500

    Lesson 5

---
+++ war-challenge/war_challengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  war_challengeApp.swift
+//  war-challenge
+//
+//  Created by Blaine Garrett on 7/24/21.
+//
+
+import SwiftUI
+
+@main
+struct war_challengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
